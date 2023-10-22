638bbe18db2d24343c87b33a2868b6e1a84be466
antoninbrdl24 <antonin.brondel24@gmail.com>
2023-10-22 14:03:16 +0200

    cette quête de merde est fini

--- data-pico.php
+++ data-pico.php
@@ -1,21 +1,21 @@
 <?php
 $articles=$cardCarousel=[
     [
-    'image'=>"",
+    'image'=>"https://picsum.photos/id/103//300/200",
     'title'=> 'Visit component classless',
     'description'=>" Some quick example text to build on the card title and make up the bulk of the card's content."
     ],
     [
-        'image'=>"",
+        'image'=>"https://picsum.photos/id/114//300/200",
         'title'=> 'Facing cascade Style Sheet',
     'description'=>"Some quick example test"],
     [
-        'image'=>"",
+        'image'=>"https://picsum.photos/id/113//300/200",
         'title'=> 'Contemplate Form Inputs river',
     'description'=>"Make up the bulk of the card's content."
     ],
 [
-    'image'=>"",
+    'image'=>"https://picsum.photos/id/117//300/200",
     'title'=> 'Enjoy no Javascript',
 'description'=>"Build on the card title and make up the bulk of the card's content."
 ]
--- pico-quest.php
+++ pico-quest.php
@@ -5,48 +5,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Discovering Pico.css</title>
-    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"> 
     <link rel="stylesheet" href="style.css" />
     <?php include 'data-pico.php'?>
   </head>
 
   <body>
-    <nav>
+  <nav>
       <ul>
-        <li class="title-li"><strong>Discovering Pico</strong></li>
+        <li ><a><strong>Discovering Pico</strong</a></li>
       </ul>
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
-        <li>
-      <details role="list">
-        <summary aria-haspopup="listbox" role="link">Categories</summary>
-        <ul role="listbox">
-          <li><a>Photos</a></li>
-          <li><a>Articles</a></li>
-          <li><a>Projetcs</a></li>
-        </ul>
-      </details>
-      </li>
+        
+          <details role="list">
+            <summary aria-haspopup="listbox" role="button" >Categories</summary>
+            <ul role="listbox">
+              <li><a>Photos</a></li>
+              <li><a>Articles</a></li>
+              <li><a>Projetcs</a></li>
+            </ul>
+          </details>
       </ul>
-    </nav>
+  </nav>
+    <main class="container-fluid">
+   
     <div>
-      <h1>Journey into Pico.css<h1>
-      <a href="#">https://picocss.com</a>
+      <h2>Journey into Pico.css<h2>
+      <h3><a href="#">https://picocss.com</a></h3>
     </div>
-    <div class="grid">
+    <section class="grid">
     <?php foreach($articles as $title=>$article): ?>
     <article>
-      <img  src="<?= $article['image']?>">
-      <header><?= $article['title']?></header>
-      <?= $article['description'] ?>
-      <footer><a href="#">Read more</a></footer>
+      <header><img src="<?= $article['image']?>"></header>
+      <h4><?= $article['title']?></h4>
+      <p><?= $description=$article['description'];?></p>
+
+      <footer>Read more</footer>
     </article>
       <?php endforeach ?>
-    </div>
+    </section>
+  
+
+    </main>
     <div class="footer">
-    </div>
-
-
+      
+      <h4>The CSS Frameworks are very useful</h4>
+      <p>Learning them comes with practice</p>
+      <footer>&copy; Wilders | images @picsum</footer>
+  </div>
   </body>
 </html>
